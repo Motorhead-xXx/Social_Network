ef94be3381dc9b6db6e1e271fcbcf85110056429
Motorhead <masa_i@bk.ru>
2021-08-13 21:55:11 +0300

    Creat new commit

---
+++ src/components/Content.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Content = () => {
+    return <div className='content'>
+        <img className='images2' src="https://lifeo.ru/wp-content/uploads/vesna-100-min.jpg"/>
+        <div>
+            ava + discription
+            Bla bla bal
+        </div>
+
+        <div>
+            New post
+        </div>
+        <div>
+            <div>
+                post1
+            </div>
+            <div>
+                post2
+            </div>
+            <div>
+                post3
+            </div>
+        </div>
+    </div>
+}
+
+export default Content;
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img className='images1'
+             src='https://f0.pngfuel.com/png/197/646/farmers-market-logo-kuna-farmers-market-marketplace-png-clip-art.png'/>
+    </header>
+}
+
+export default Header;
